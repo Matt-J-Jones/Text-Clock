5da6b355c3a32cfc4feeeb3a0e145830def4252f
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-07-03 14:27:12 +0100

    added text and css for colours

--- src/App.js
+++ src/App.js
@@ -5,18 +5,73 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div class="text-container">
+          <div class="lineOne">
+            <div class="itis">IT</div>
+            <div class="letter">L</div>
+            <div class="itis">IS</div>
+            <div class="letter">AS</div>
+            <div class="am">AM</div>
+            <div class="pm">PM</div>
+          </div>
+
+          <div class="lineTwo">
+            <div class="aquarter">A</div>
+            <div class="letter">C</div>
+            <div class="aquarter">QUARTER</div>
+            <div class="letter">DC</div>
+          </div>
+
+          <div class="lineThree">
+            <div class="twenty">TWENTY</div>
+            <div class="five">FIVE</div>
+            <div class="letter">X</div>
+          </div>
+
+          <div class="lineFour">
+            <div class="half">HALF</div>
+            <div class="letter">S</div>
+            <div class="ten">TEN</div>
+            <div class="letter">F</div>
+            <div class="to">TO</div>
+          </div>
+
+          <div class="lineFive">
+            <div class="past">PAST</div>
+            <div class="letter">ERU</div>
+            <div class="nineNumber">NINE</div>
+          </div>
+
+          <div class="lineSix">
+            <div class="oneNumber">ONE</div>
+            <div class="sixNumber">SIX</div>
+            <div class="threeNumber">THREE</div>
+          </div>
+
+          <div class="lineSeven">
+            <div class="fourNumber">FOUR</div>
+            <div class="fiveNumber">FIVE</div>
+            <div class="twoNumber">TWO</div>
+          </div>
+
+          <div class="lineEight">
+            <div class="eightNumber">EIGHT</div>
+            <div class="elevenNumber">ELEVEN</div>
+          </div>
+
+          <div class="lineNine">
+            <div class="sevenNumber">SEVEN</div>
+            <div class="twelveNumber">TWELVE</div>
+          </div>
+
+          <div class="lineTen">
+          <div class="tenNumber">TEN</div>
+          <div class="letter">SE</div>
+          <div class="oclock">OCLOCK</div>
+          </div>
+          
+        </div>
       </header>
     </div>
   );
